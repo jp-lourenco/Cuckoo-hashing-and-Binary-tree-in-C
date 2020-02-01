e63b8734d26e8582a36334a3aa7495688af9af58
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-02-01 10:36:21 -0300

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,200 @@
+#include "biblioteca1.c"
+
+int main(){
+	
+	FILE *ptr;
+	int i,j, cont,B=1,k=1;
+	registro myStruct[4];
+	double ti,tf,tempo;
+	struct timeval tempo_inicio, tempo_fim;
+	abp *pRaiz = NULL;	
+	campo ***tabela;
+	unsigned long long A;	
+	
+	while(1){
+		
+			
+	do {
+		if(B!=1)
+		printf("\nValor invalido! Digite novamente.\n");		
+		printf(" ______________________________________\n");
+		printf("|_________________Menu_________________|\n");
+		printf("| Digite [1]:Cuckoo hash               |\n");
+		printf("| Digite [2]:Arvore Binaria de Pesquisa|\n");
+		printf("| Digite [0]:Sair                      |\n");	
+		printf("|______________________________________|\n");
+		printf("\nDigite a entrada: ");
+		scanf("%i",&B);
+		setbuf(stdin,NULL);
+	}while(B!=0 && B!=1 && B!=2);
+		switch(B){
+
+			case 1:{				
+				k=1;				
+				printf("\nConstruindo tabela hash....\n");
+				ptr = fopen("base.bin","rb");
+	
+				tabela =malloc(N * sizeof(campo**));
+		
+				if (!ptr) {
+					  printf("\nO arquivo nao pode ser aberto\n");
+					  return 1;
+				 }
+
+				for (i = 0; i < N; i++){
+					tabela[i] = malloc(TAM * sizeof(campo*)); 
+					 for (j = 0; j < TAM; j++){
+						  tabela[i][j] = NULL; //Inicializa com NULL.
+					 }
+				}
+
+				i=0;
+				while(i<1000000) {
+					  fread(&myStruct,sizeof(registro)*4,1,ptr);
+					  if(feof(ptr)!=0)
+						   break;
+					  // percorre os 4 registros do bloco lido e insere um por um na ABP
+					  for(cont=0;cont<4;cont++) {
+					  	InserirHash(tabela,myStruct[cont].naochave,i,0,0);
+					}
+					  i++;
+				 }
+		
+				while(1){			
+					if(k==0){		
+						do{						
+							if(B!=1)
+								printf("Valor invalido! Digite novamente.\n");		
+							printf(" ____________________________\n");
+							printf("|__________Menu Hash_________|\n");
+							printf("| Digite [1]:Buscar novamente|\n");
+							printf("| Digite [0]:Sair            |\n");	
+							printf("|____________________________|\n");
+							printf("\nDigite a entrada: ");
+							scanf("%i",&B);
+							setbuf(stdin,NULL);	
+						}while(B!=0 && B!=1);					
+					}		
+					switch(B){	
+						
+						case 1:{	
+							printf("\nDigite o numero que deseja buscar: ");
+							scanf("%llu",&A);
+							setbuf(stdin,NULL);
+							ti = tf = tempo = 0;
+										
+							gettimeofday(&tempo_inicio,NULL);
+
+							PesquisarHash(tabela,A);	
+	
+							gettimeofday(&tempo_fim,NULL);
+							tf = (double) tempo_fim.tv_usec + ((double)tempo_fim.tv_sec * (1000000.0));
+							ti = (double) tempo_inicio.tv_usec + ((double)tempo_inicio.tv_sec * (1000000.0));
+							tempo = (tf - ti) / 1000;
+							printf("\nTempo gasto (milisegundos) para pesquisar: %lf\n", tempo);
+							k=0;
+							continue;
+						}
+					 
+						case 0:{
+							B=1;							
+							break;
+						}
+					}
+					printf("\nDestruindo tabela hash....\n");
+					for (i = 0; i < N; i++){ 
+				 		for (j = 0; j < TAM; j++)
+				  			free(tabela[i][j]) ;
+			 			free(tabela[i]);
+					}
+					free(tabela);	
+					break;	
+				}		
+				continue;
+			}	
+			
+
+			case 2:{
+				B=B-1;				
+				k=1;				
+				printf("\nConstruindo ABP....\n");	
+				ptr = fopen("base.bin","rb");
+			 
+				 if(!ptr){
+					  printf("\nO arquivo nao pode ser aberto\n");
+					  return 1;
+				 }
+				 
+				i=0;
+		
+		
+		
+				// Cada while le um bloco de 4096 - ou seja - 4 registros
+				while(i<1000000) {
+					fread(&myStruct,sizeof(registro)*4,1,ptr);
+						if(feof(ptr)!=0)
+						   break;
+					  // percorre os 4 registros do bloco lido e insere um por um na ABP
+					for(cont=0;cont<4;cont++) {
+				  		inserirABP(&pRaiz,myStruct[cont].naochave,i);
+					}
+					i++;
+				}
+
+				fclose(ptr);
+				 
+				 // Comeca a contar o tempo
+				ti = tf = tempo = 0;
+				while(1){			
+					if(k==0){		
+						do{	
+							if(B!=1)
+								printf("\nValor invalido! Digite novamente.\n");		
+							printf(" ____________________________\n");
+							printf("|___________Menu ABP_________|\n");
+							printf("| Digite [1]:Buscar novamente|\n");
+							printf("| Digite [0]:Sair            |\n");	
+							printf("|____________________________|\n");
+							printf("\nDigite a entrada: ");											
+							scanf("%i",&B);
+							setbuf(stdin,NULL);	
+						}while(B!=0 && B!=1);
+					}		
+					switch(B){	
+			
+						case 1:{	
+							printf("\nDigite o numero que deseja buscar: ");
+							scanf("%llu",&A);
+							
+							gettimeofday(&tempo_inicio,NULL);
+	
+							// Faz a pesquisa
+							PesquisarABP(pRaiz,A);
+	
+							// Para de contar o tempo e o imprime
+							gettimeofday(&tempo_fim,NULL);
+							tf = (double) tempo_fim.tv_usec + ((double)tempo_fim.tv_sec * (1000000.0));
+							ti = (double) tempo_inicio.tv_usec + ((double)tempo_inicio.tv_sec * (1000000.0));
+							tempo = (tf - ti) / 1000;
+							printf("\nTempo gasto (milisegundos) para pesquisar: %lf\n", tempo);
+							k=0;
+							continue;
+						}
+						case 0:{							
+							B=1;
+							break;
+						}	 
+
+					}		
+				printf("\nDestruindo ABP....\n");
+				ApagaArvore(&pRaiz);	
+				pRaiz = NULL;				
+				break;
+			}
+			continue;
+		}
+
+			case 0:return 0;
+		}
+	}
+}
---
+++ biblioteca1.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+#define N 7//NUMEROS DE FUNCÕES E TABELAS QUE O CHUCKOO HASH IMPLEMENTADO POSSUI 
+#define TAM 720059	//TAMANHO DAS TABELAS
+#define _FILE_OFFSET_BITS 64
+
+typedef struct registro {
+	unsigned long long chave; // 8 bytes
+	unsigned long long naochave; // 8 bytes
+	unsigned char outros[1008]; // 1008 bytes
+} registro;
+
+typedef struct no {
+	unsigned int bloco;
+	struct no *prox;
+} no;
+
+typedef struct abp {
+	unsigned long long naochave;
+	struct abp *esq, *dir;
+	struct no *lista;
+} abp;
+
+typedef struct campo {
+	unsigned long long naochave;
+	int bloco;
+} campo;
+
+
+
+void inserirABP(abp **pRaiz, unsigned long long valor, unsigned int bloco) {
+	
+   if (*pRaiz == NULL) {
+       *pRaiz = malloc(sizeof(abp));
+       (*pRaiz)->esq = NULL;
+       (*pRaiz)->dir = NULL;
+       (*pRaiz)->naochave = valor;
+       (*pRaiz)->lista = malloc(sizeof(no));
+       (*pRaiz)->lista->prox = NULL;
+       (*pRaiz)->lista->bloco = bloco;
+   } else {
+       if(valor < (*pRaiz)->naochave) {
+           inserirABP(&(*pRaiz)->esq, valor,bloco);
+        }
+       if(valor > (*pRaiz)->naochave) {
+           inserirABP(&(*pRaiz)->dir, valor,bloco);
+        }
+        if(valor == (*pRaiz)->naochave) {
+        	no *no = (*pRaiz)->lista;
+        	while(no->prox != NULL) {
+        		no = no->prox;
+			}
+			
+			struct no *novo = malloc(sizeof(*no));
+			novo->prox = NULL;
+			novo->bloco = bloco;
+			no->prox = novo;
+		}
+   }
+}
+
+void PesquisarABP(abp *pRaiz, unsigned long long valor) {
+   if(pRaiz != NULL) {
+   		// Caso tenha encontrado o valor CHAVE
+   		if (pRaiz->naochave == valor) {
+   			// Abre o arquivo para ler os dados dos valores CHAVE
+			FILE *ptr;
+			int i;
+			registro dados[4];
+			int bloco,auxbloco=0;
+			ptr = fopen("base.bin","rb");
+   			// Percorre a lista que contem esses valores como CHAVE
+   			no *aux = pRaiz->lista;
+   			while(aux != NULL) {
+					auxbloco=0;					
+					bloco = aux->bloco;   				
+					if(bloco>500000){
+						auxbloco=bloco-500000;
+						bloco=bloco-auxbloco;
+					}		
+					if(fseek(ptr, bloco*4096 , SEEK_SET) == 0){//COLOCA O CABEÇOTE NO BLOCO QUE ESTA O REGISTRO 
+						if(bloco!=0)
+							if(fseek(ptr, auxbloco*4096 , SEEK_CUR) == 0)
+   					// Le o bloco do HD na posicao do bloco e armazena no vetor dados
+   					fread(&dados,sizeof(registro)*4, 1 ,ptr);
+   					// Percorre o vetor de dados do bloco
+		   			for(i=0;i<4;i++) {
+		   			// Quando encontrar um valor do vetor com o naochave igual -> imprime todos os dados do registro
+		   				if (dados[i].naochave == valor) {
+		   					printf("\nChave: %llu",dados[i].chave);
+								printf("\nNaoChave: %llu",dados[i].naochave);
+								printf("\nOutros: %s",dados[i].outros);
+								printf("\n");		
+							}
+						}
+					}								
+					aux = aux->prox;
+				}
+			fclose(ptr);
+   			return;
+		}
+   		if(pRaiz->naochave > valor) {
+   				return PesquisarABP(pRaiz->esq,valor);
+		} else {
+				return PesquisarABP(pRaiz->dir,valor);
+		}
+   }
+   printf("\n\nValor Chave Nao Encontrado!!\n");
+	return;
+}
+void ApagaArvore (abp **raiz) {
+	if ((*raiz) != NULL){
+	ApagaArvore (&(*raiz)->esq) ; 
+	ApagaArvore (&(*raiz)->dir) ;
+  	free(*raiz);
+  	}
+}
+
+int hash(int funcao, unsigned long long chave){//FUNÇÕES DO CUCKOO HASH  
+	if(chave<0)//CASO O VALOR SEJA NEGATIVO TIRA O MODULO PARA ACHAR O INDICE
+		chave=chave*-1;    
+	switch (funcao){
+     	case 0: return (chave/337)%TAM;						/*RETORNA O INDICE*/
+      case 1: return (chave/10003)%TAM;
+		case 2: return (chave/11)%TAM;
+      case 3: return (chave/23)%TAM;    
+		case 4: return (chave/17)%TAM;
+		case 5: return (chave/TAM)%TAM;
+ 		case 6: return chave%TAM;   	
+	}
+}
+
+
+void PesquisarHash(campo*** tabela, unsigned long long naochave) {
+	int i, indice,bloco,j,cont=0,auxbloco=0;	
+	FILE *ptr;
+	registro dados[4];
+	ptr = fopen("base.bin","rb");
+	
+	for(i=0;i<N;i++){//BUSCA O ELEMENTO EM TODOS OS INDICES DO MESMO
+		auxbloco=0;		
+		indice=hash(i,naochave);
+		if(tabela[i][indice]!=NULL){					  //CASO TENHA ALGUM ELEMENTO 		
+			if(tabela[i][indice]->naochave==naochave)//VERIFICA SE O ELEMENTO DESEJADO
+				bloco=tabela[i][indice]->bloco;		  //SE FOR, GUARDAMOS O VALOR DO BLOCO
+			else//CASO NÃO SEJA O ELEMENTO DESEJADO
+				continue;
+		}else//CASO NÃO TENHA NENHUM ELEMENTO NA POSICÃO 
+			continue;	
+		if(bloco>500000){
+			auxbloco=bloco-500000;
+			bloco=bloco-auxbloco;
+		}		
+		if(fseek(ptr, bloco*4096 , SEEK_SET) == 0){//COLOCA O CABEÇOTE NO BLOCO QUE ESTA O REGISTRO 
+			if(bloco!=0)
+				if(fseek(ptr, auxbloco*4096 , SEEK_CUR) == 0){
+			fread(&dados,sizeof(registro), 4 ,ptr);// LE O BLOCO QUE CONTEM O REGISTRO
+				for(j=0;j<4;j++) {// PERCORRE O VETOR DE DADOS DO BLOCO
+					if (dados[j].naochave == naochave) {// QUANDO ENCONTRAR UM VALOR DO VETOR COM O NAOCHAVE IGUAL -> IMPRIME TODOS OS	DADOS DO REGISTRO
+						printf("\nChave: %llu",dados[j].chave);
+						printf("\nNaoChave: %llu",dados[j].naochave);
+						printf("\nOutros: %s",dados[j].outros);
+						printf("\n");		
+						cont++;												
+					}	
+				}
+			}
+		}	
+	}
+	if(cont==0)
+		printf("\n\nValor Chave Nao Encontrado!!\n");
+	return;
+}
+void InserirHash(campo*** tabela,unsigned long long naochave, int bloco, int funcao,int rehash){		
+	if(rehash<10000){//CASO NÃO CONSIGA INSERIR NA TABELA(LOOP INFINITO "REHASH")		
+		
+	int indice=hash(funcao,naochave);//JOGA O VALOR NAOCHAVE NA FUNÇÃO HASH	
+		
+		if(tabela[funcao][indice]==NULL){						//VERIFICA SE NÃO TEM NADA NA POSIÇÃO,
+			tabela[funcao][indice]=malloc(sizeof(campo));	//CASO NÃO TENHA ALOCA NO INDICE O ELEMENTO
+			tabela[funcao][indice]->naochave=naochave;
+			tabela[funcao][indice]->bloco=bloco;
+			return;	
+		
+		}else{															//TEM ELEMENTO NA POSIÇÃO
+			campo *aux=tabela[funcao][indice];					//GUARDA ELE EM UM AUX
+			tabela[funcao][indice]=malloc(sizeof(campo));	//ALOCA O ELEMENTO NA POSIÇÃO DO INDICE
+			tabela[funcao][indice]->naochave=naochave;		//COLOCA O VALOR NÃOCHAVE
+			tabela[funcao][indice]->bloco=bloco;				//COLOCA O BLOCO DO REGISTRO QUE POSSUI O VALOR NAOCHAVE
+			funcao=(funcao+1)%N;										//PASSA PRA PROXIMA TABELA
+			rehash++;														
+			return InserirHash(tabela, aux->naochave, aux->bloco, funcao,rehash);//JOGA O ELEMENTO DO AUX PARA TENTAR INSERIR NA PROXIMA TABELA
+		}
+	}	
+	printf("rehash\n");
+	return;
+}
